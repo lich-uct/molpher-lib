6dc8386b19849aa8fa18cc225177b8c906dce516
martin-sicho <sichom@vscht.cz>
2015-12-16 13:53:26 +0100

    added makefiles to the repository so that netbeans is not necessary for build

---
+++ backend/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Linux64_Release configuration
+CND_PLATFORM_Linux64_Release=GNU_4_6-Linux-x86
+CND_ARTIFACT_DIR_Linux64_Release=dist/Linux64_Release/GNU_4_6-Linux-x86
+CND_ARTIFACT_NAME_Linux64_Release=molpher-srv
+CND_ARTIFACT_PATH_Linux64_Release=dist/Linux64_Release/GNU_4_6-Linux-x86/molpher-srv
+CND_PACKAGE_DIR_Linux64_Release=dist/Linux64_Release/GNU_4_6-Linux-x86/package
+CND_PACKAGE_NAME_Linux64_Release=backend.tar
+CND_PACKAGE_PATH_Linux64_Release=dist/Linux64_Release/GNU_4_6-Linux-x86/package/backend.tar
+# Linux64_Debug_library configuration
+CND_PLATFORM_Linux64_Debug_library=GNU_4_6-Linux-x86
+CND_ARTIFACT_DIR_Linux64_Debug_library=dist/Linux64_Debug_library/GNU_4_6-Linux-x86
+CND_ARTIFACT_NAME_Linux64_Debug_library=libbackend.a
+CND_ARTIFACT_PATH_Linux64_Debug_library=dist/Linux64_Debug_library/GNU_4_6-Linux-x86/libbackend.a
+CND_PACKAGE_DIR_Linux64_Debug_library=dist/Linux64_Debug_library/GNU_4_6-Linux-x86/package
+CND_PACKAGE_NAME_Linux64_Debug_library=backend.tar
+CND_PACKAGE_PATH_Linux64_Debug_library=dist/Linux64_Debug_library/GNU_4_6-Linux-x86/package/backend.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ molpher-lib/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Linux64_Debug configuration
+CND_PLATFORM_Linux64_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Linux64_Debug=dist/Linux64_Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Linux64_Debug=libmolpher-lib.so
+CND_ARTIFACT_PATH_Linux64_Debug=dist/Linux64_Debug/GNU-Linux-x86/libmolpher-lib.so
+CND_PACKAGE_DIR_Linux64_Debug=dist/Linux64_Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Linux64_Debug=libmolpher-lib.so.tar
+CND_PACKAGE_PATH_Linux64_Debug=dist/Linux64_Debug/GNU-Linux-x86/package/libmolpher-lib.so.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
