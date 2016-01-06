d1aa3f764b637aa7b7e090a127d526bc0d5c205d
martin-sicho <sichom@vscht.cz>
2016-01-06 10:57:29 +0100

    extended the TreeOperation base class

--- molpher-lib/python/molpher/core/operations.py
+++ molpher-lib/python/molpher/core/operations.py
@@ -5,6 +5,8 @@ in `molpher.swig_wrappers.core`.
 
 """
 
+import molpher
+from molpher.core import ExplorationTree
 from molpher.swig_wrappers.core import \
     GenerateMorphsOper \
     , FilterMorphsOper \
@@ -13,5 +15,19 @@ from molpher.swig_wrappers.core import \
     , SortMorphsOper \
     , FindLeavesOper \
     , TraverseOper \
-    , TreeOperation \
     , TraverseCallback
+
+class TreeOperation(molpher.swig_wrappers.core.TreeOperation):
+
+    def __init__(self):
+        super(TreeOperation, self).__init__()
+        self.tree = self.getTree()
+
+    def __call__(self):
+        self.tree = self.getTree()
+
+    def getTree(self):
+        tree = super(TreeOperation, self).getTree()
+        if tree:
+            tree.__class__ = ExplorationTree # 'cast' the wrapped class to the 'pretty' Python proxy class
+        return tree
