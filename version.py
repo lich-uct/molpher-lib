809c7a51c496cb08090606bf01ed3be0ac3d2c16
martin-sicho <sichom@vscht.cz>
2016-04-21 12:32:40 +0200

    changed the way the path to the version file is detected

--- version.py
+++ version.py
@@ -1,12 +1,16 @@
+import os
+
 VERSION = None
 BUILD_NUMBER = None
 
-def load_stuff(path):
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def load_versionfile(path):
     with open(path, 'r') as versionfile:
         return versionfile.read().strip()
 
-VERSION = load_stuff("VERSION.TXT")
-BUILD_NUMBER = load_stuff("BUILD.TXT")
+VERSION = load_versionfile(os.path.join(BASE_DIR, "VERSION.TXT"))
+BUILD_NUMBER = load_versionfile(os.path.join(BASE_DIR, "BUILD.TXT"))
 
 print('Version: ', VERSION)
 print('Build: ', BUILD_NUMBER)
