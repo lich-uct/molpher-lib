a0d2ca85986d8f714d34f85c15a0c00ef0aad813
martin-sicho <sichom@vscht.cz>
2016-04-25 16:52:38 +0200

    the seed is now intilized randomly

--- src/core/misc/SynchRand.h
+++ src/core/misc/SynchRand.h
@@ -26,7 +26,7 @@
 
 // [0,1] - enable for debugging
 #ifndef SYNCHRAND_DETERMINISTIC
-#define SYNCHRAND_DETERMINISTIC 1 // TODO: SET TO 0 IN RELEASES!!!
+#define SYNCHRAND_DETERMINISTIC 0 // TODO: SET TO 0 IN RELEASES!!!
 #endif
 
 // [0..3] - 0 most precise, 3 fastest
