d2e9a22ed574e1d9921fc3bc72f1396e43af063b
martin-sicho <sichom@vscht.cz>
2016-05-02 18:57:26 +0200

    - moved tbb dependency to a separate package - added test script to molpher-lib package

--- build_conda.py
+++ build_conda.py
@@ -6,14 +6,17 @@ from jinja2 import Template
 import version
 
 BUILD_ALL_PYTHON = False
-TARGETS = ["molpher-lib"]
+TARGETS = ["tbb", "molpher-lib"]
 PYTHON_VERSIONS = ['2.6', '2.7',  '3.3',  '3.4', '3.5'] if BUILD_ALL_PYTHON else ['3.5', '2.7']
 VERSION = version.VERSION
 BUILD_NUMBER = version.BUILD_NUMBER
 LICENSE_FILE = "LICENSE.txt"
+TBB_LICENSE_FILE = "deps/tbb/COPYING"
+TBB_VERSION = "4.2"
 
 for target in TARGETS:
-    BASE_DIR = os.path.abspath(".")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(BASE_DIR)
     for python_version in PYTHON_VERSIONS:
         PACKAGE_DIR = os.path.join(BASE_DIR, "conda/{0}/{0}/".format(target))
         METAFILE_TEMPLATE_PATH = os.path.join(PACKAGE_DIR, "../meta.yaml.template")
@@ -31,18 +34,28 @@ for target in TARGETS:
                 , license_file=LICENSE_FILE
                 , build_string="py{0}_{1}".format(python_version.replace('.', ''), BUILD_NUMBER)
                 , python_spec="python {0}*".format(python_version)
+                , tbb_version=TBB_VERSION
+                , tbb_spec = "tbb {0}*".format(TBB_VERSION)
             ))
-
-        copyfile(os.path.join(BASE_DIR, LICENSE_FILE), os.path.join(PACKAGE_DIR, LICENSE_FILE))
 
         os.chdir(os.path.join(PACKAGE_DIR, "../"))
         os.environ['BASE_DIR'] = BASE_DIR
-        ret = subprocess.call("conda build {0} --python {1}".format(target, python_version), env=os.environ, shell=True)
+        if target == 'tbb':
+            # do not set Python version for tbb
+            copyfile(os.path.join(BASE_DIR, TBB_LICENSE_FILE), os.path.join(PACKAGE_DIR, LICENSE_FILE))
+            ret = subprocess.call("conda build {0}".format(target), env=os.environ, shell=True)
+        else:
+            copyfile(os.path.join(BASE_DIR, LICENSE_FILE), os.path.join(PACKAGE_DIR, LICENSE_FILE))
+            ret = subprocess.call("conda build {0} --python {1}".format(target, python_version), env=os.environ, shell=True)
         if ret != 0:
-            print("Error has occured during conda build...")
-            exit()
+            print("An error has occured during conda build...")
+            exit(1)
 
         # remove the generated and copied files and go back to BASE_DIR
         os.remove(METAFILE_PATH)
         os.remove(os.path.join(PACKAGE_DIR, LICENSE_FILE))
         os.chdir(BASE_DIR)
+
+        # make sure that the tbb package is only built once
+        if target == 'tbb':
+            break
--- conda/molpher-lib/molpher-lib/build.sh
+++ conda/molpher-lib/molpher-lib/build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# build and install the library
+# build the C++ library
 cd $BASE_DIR
 pwd
-make -f Makefile CONF=Release_Linux_amd64
+make -f Makefile -j `grep -c ^processor /proc/cpuinfo` CONF=Release_Linux_amd64
 
+# install the C++ library
 LIB_DIR="$PREFIX/lib/"
 mkdir -p $LIB_DIR
-cp dist/lib/* $LIB_DIR
+cp dist/lib/libmolpher.* $LIB_DIR
 
 INCLUDE_DIR="$PREFIX/include/molpher/"
 mkdir -p $INCLUDE_DIR
-cp -r include/* $INCLUDE_DIR
+rsync -avz --progress include/ $INCLUDE_DIR --exclude '*.i'
 
-# build the bindings and install the package
+# build the bindings and install the Python package
 $PYTHON setup.py install
