b2f8c02b59d5be2465c2a60f23c7def42054430f
martin-sicho <sichom@vscht.cz>
2016-05-03 16:58:45 +0200

    changed the build script to be more universal

--- conda/tbb/tbb/build.sh
+++ conda/tbb/tbb/build.sh
@@ -4,9 +4,10 @@ cd $BASE_DIR
 
 LIB_DIR="$PREFIX/lib/"
 mkdir -p $LIB_DIR
-cp dist/lib/libtbbmalloc_proxy.* $LIB_DIR
-cp dist/lib/libtbbmalloc.* $LIB_DIR
-cp dist/lib/libtbb.* $LIB_DIR
+TBB_LIBS_DIR="${BASE_DIR}/deps/tbb/lib/intel64/gcc4.4/"
+cp ${TBB_LIBS_DIR}/libtbbmalloc_proxy.* $LIB_DIR
+cp ${TBB_LIBS_DIR}/libtbbmalloc.* $LIB_DIR
+cp ${TBB_LIBS_DIR}/libtbb.* $LIB_DIR
 
 INCLUDE_DIR="$PREFIX/include/tbb/"
 mkdir -p $INCLUDE_DIR
