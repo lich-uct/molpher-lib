f2bf43393ff2c02eb3d265e6117beeabf9a809a8
martin-sicho <sicho.martin@gmail.com>
2016-07-13 14:22:06 +0200

    implemented the antifingerprint computation for generated paths

--- src/python/molpher/examples/antidecoys.py
+++ src/python/molpher/examples/antidecoys.py
@@ -1,13 +1,20 @@
-"""
-Implementation of the bidirectional search algorithm.
-
-"""
-
+import os
+import pickle
 import time
+
+from rdkit import RDConfig
+from rdkit.Chem import ChemicalFeatures
+
+from rdkit import Chem
+from rdkit.Chem.Pharm2D import Generate
+from rdkit.Chem.Pharm2D.SigFactory import SigFactory
 
 from molpher.core.ExplorationTree import ExplorationTree as ETree
 from molpher.core.operations import *
 from molpher.core.selectors import *
+from molpher import random
+
+random.set_random_seed(42)
 
 def timeit(func):
     milliseconds = 1000 * time.clock()
@@ -28,9 +35,22 @@ class FindClosest:
         if morph_dist < current_dist:
             self.closest = morph.copy()
 
+class AntidecoysFilter(TreeOperation):
+
+    def __init__(self):
+        super(AntidecoysFilter, self).__init__()
+        self.antifingerprint = None
+
+    def __call__(self):
+        if self.antifingerprint:
+            # TODO: implement
+            pass
+
 class BidirectionalPathFinder:
 
-    def __init__(self, source, target):
+    def __init__(self, source, target, verbose=True, antidecoys_filter=AntidecoysFilter()):
+        self.antidecoys_filter = antidecoys_filter
+        self.verbose = verbose
         options = {
             'fingerprint' : FP_ATOM_PAIRS
         }
@@ -44,6 +64,7 @@ class BidirectionalPathFinder:
             GenerateMorphsOper()
             , SortMorphsOper()
             , FilterMorphsOper()
+            , self.antidecoys_filter
             , ExtendTreeOper()
             , PruneTreeOper()
         ]
@@ -68,27 +89,27 @@ class BidirectionalPathFinder:
             counter+=1
             print('Iteration {0}:'.format(counter))
             for oper in self.ITERATION:
-                print('Execution times ({0}):'.format(type(oper).__name__))
+                print('Execution times ({0}):'.format(type(oper).__name__)) if self.verbose else None
 
                 source_target_time = timeit(lambda : self.source_target.runOperation(oper))
-                print('\tsource -> target: {0}'.format(source_target_time))
+                print('\tsource -> target: {0}'.format(source_target_time)) if self.verbose else None
                 target_source_time = timeit(lambda : self.target_source.runOperation(oper))
-                print('\ttarget -> source: {0}'.format(target_source_time))
+                print('\ttarget -> source: {0}'.format(target_source_time)) if self.verbose else None
 
-                print('\ttotal time: {0}'.format(source_target_time + target_source_time))
+                print('\ttotal time: {0}'.format(source_target_time + target_source_time)) if self.verbose else None
 
-            print('Traversal times:')
+            print('Traversal times:') if self.verbose else None
 
             source_target_time = timeit(lambda : self.source_target.traverse(self.source_target_min))
-            print('\tsource -> target: {0}'.format(source_target_time))
+            print('\tsource -> target: {0}'.format(source_target_time)) if self.verbose else None
             target_source_time = timeit(lambda : self.target_source.traverse(self.target_source_min))
-            print('\ttarget -> source: {0}'.format(target_source_time))
+            print('\ttarget -> source: {0}'.format(target_source_time)) if self.verbose else None
 
-            print('\ttotal time: {0}'.format(source_target_time + target_source_time))
+            print('\ttotal execution time: {0}'.format(source_target_time + target_source_time))
 
-            print('Current Targets:')
-            print('\tsource to target:', self.source_target.params['target'])
-            print('\ttarget to source:', self.target_source.params['target'])
+            print('Current Targets:') if self.verbose else None
+            print('\tsource to target:', self.source_target.params['target']) if self.verbose else None
+            print('\ttarget to source:', self.target_source.params['target']) if self.verbose else None
 
             print('Current Minima:')
             print('\tsource to target:', self.source_target_min.closest.getSMILES(), self.source_target_min.closest.getDistToTarget())
@@ -101,21 +122,21 @@ class BidirectionalPathFinder:
                 'target' : self.source_target_min.closest.getSMILES()
             }
 
-            print('New Targets:')
-            print('\tsource to target:', self.source_target.params['target'])
-            print('\ttarget to source:', self.target_source.params['target'])
+            print('New Targets:') if self.verbose else None
+            print('\tsource to target:', self.source_target.params['target']) if self.verbose else None
+            print('\ttarget to source:', self.target_source.params['target']) if self.verbose else None
 
             if self.source_target.path_found:
-                print('Path Found in tree going from source to target:')
+                print('Path Found in tree going from source to target') if self.verbose else None
                 connecting_molecule = self.source_target.params['target']
-                print('Connecting molecule:', connecting_molecule)
+                print('Connecting molecule:', connecting_molecule) if self.verbose else None
                 assert self.source_target.hasMol(connecting_molecule)
                 assert self.target_source.hasMol(connecting_molecule)
                 break
             if self.target_source.path_found:
-                print('Path Found in tree going from target to source:')
+                print('Path Found in tree going from target to source') if self.verbose else None
                 connecting_molecule = self.target_source.params['target']
-                print('Connecting molecule:', connecting_molecule)
+                print('Connecting molecule:', connecting_molecule) if self.verbose else None
                 assert self.target_source.hasMol(connecting_molecule)
                 assert self.source_target.hasMol(connecting_molecule)
                 break
@@ -126,16 +147,44 @@ class BidirectionalPathFinder:
         target_source_path.reverse()
         source_target_path.extend(target_source_path)
         self.path = source_target_path
+        print('Path:', self.path)
+        return self.path
+
+
+def antifingerprint_from_paths(path, antifingerprint=None):
+    fdef_name = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')  # get basic feature definitions
+    feature_factory = ChemicalFeatures.BuildFeatureFactory(fdef_name)  # make feature factory
+    sign_fac = SigFactory(feature_factory, minPointCount=2, maxPointCount=3)
+    sign_fac.SetBins([(0,2),(2,5),(5,8)])
+    sign_fac.Init()
+
+    antifingerprint = antifingerprint
+    for smiles in path:
+        mol = Chem.MolFromSmiles(smiles)
+        if not antifingerprint:
+            antifingerprint = Generate.Gen2DFingerprint(mol, sign_fac)
+        else:
+            antifingerprint = antifingerprint | Generate.Gen2DFingerprint(mol, sign_fac)
+
+    return antifingerprint
 
 def main():
     milliseconds_now = 1000 * time.clock()
     cocaine = 'CN1[C@H]2CC[C@@H]1[C@@H](C(=O)OC)[C@@H](OC(=O)c1ccccc1)C2'
     procaine = 'O=C(OCCN(CC)CC)c1ccc(N)cc1'
 
-    pathfinder = BidirectionalPathFinder(cocaine, procaine)
-    pathfinder()
-    print(pathfinder.path)
-    print('Total Execution Time: {0}'.format(1000 * time.clock() - milliseconds_now))
+    paths = []
+    antidecoys_filter = AntidecoysFilter()
+    for i in range(10):
+        # find a path
+        pathfinder = BidirectionalPathFinder(cocaine, procaine, verbose=False, antidecoys_filter=antidecoys_filter)
+        paths.append(pathfinder.path)
+        pickle.dump(paths, open('paths.pickle'.format(i)), mode='bw')
+        print('Total Execution Time (search #{1}): {0}'.format(1000 * time.clock() - milliseconds_now, i + 1))
+
+        # compute and save new antifingerprint
+        antidecoys_filter.antifingerprint = antifingerprint_from_paths(pathfinder.path, antidecoys_filter.antifingerprint)
+
 
 if __name__ == "__main__":
     exit(main())
