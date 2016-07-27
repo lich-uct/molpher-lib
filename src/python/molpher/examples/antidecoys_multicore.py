b029c0195ae45277bb99782df481fca1e2378ec4
martin-sicho <sicho.martin@gmail.com>
2016-07-27 15:58:40 +0200

    deleted obsolete file

--- src/python/molpher/examples/antidecoys_multicore.py
+++
@@ -1,9 +0,0 @@
-from molpher.algorithms.antidecoys.core import run
-
-def main():
-    cocaine = 'CN1[C@H]2CC[C@@H]1[C@@H](C(=O)OC)[C@@H](OC(=O)c1ccccc1)C2'
-    procaine = 'O=C(OCCN(CC)CC)c1ccc(N)cc1'
-    run(cocaine, procaine)
-
-if __name__ == "__main__":
-    exit(main())
