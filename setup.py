a53f2180b930a4f30b121d4d638bbf0139dfcfc6
martin-sicho <sicho.martin@gmail.com>
2016-08-23 15:16:22 +0200

    libraries are now linked properly

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ from version import VERSION, BUILD_NUMBER
 molpher_cpp_module = Extension('molpher.swig_wrappers._core',
                            sources=[os.path.abspath('src/swig/molpher_wrap.cpp')],
                            include_dirs = [os.path.abspath('include/')],
-                           library_dirs=[os.path.abspath('dist/lib/')],
+                           # library_dirs=[os.path.abspath('dist/lib/')],
                            libraries=['molpher'],
-                           runtime_library_dirs=[os.path.abspath('dist/lib/')],
+                           # runtime_library_dirs=[os.path.abspath('dist/lib/')],
                            extra_compile_args=['-std=c++11'],
                            language='c++'
                            )
