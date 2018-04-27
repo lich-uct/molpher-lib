c50f95bbc0dd9e915490ce82161ba0dc2aa2604f
martin-sicho <sicho.martin@gmail.com>
2018-04-27 16:19:20 +0200

    removed some obsolete files

--- cmake/Modules/FindMolpherLib.cmake
+++
@@ -1,6 +0,0 @@
-FIND_PATH(MOLPHER_INCLUDE_DIR data_structs/ExplorationTree.hpp
-        ${CMAKE_INCLUDE_PATH}/molpher
-        )
-FIND_LIBRARY(MOLPHER_LIBRARY molpher
-        ${CMAKE_LIBRARY_PATH}
-        )
