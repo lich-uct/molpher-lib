bc0289a0131ece49c80b571a9382f58b7bd1027a
martin-sicho <sicho.martin@gmail.com>
2018-05-03 16:40:28 +0200

    added a script to serve the built documentation on localhost directly

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -10,10 +10,20 @@ rm -f *.db
 doxygen config.cfg
 cd ..
 
-# build the Sphinx documentation
+# clean previous build
 make clean
+
+# render sample notebooks
 MOLPHER_PATH=../src/python/
+cd notebooks/
+jupyter nbconvert --to html *.ipynb
+mv *.html ../source/_static/
+cd ..
+
+# configure
 sphinx-apidoc -o source/documentation/python/ $MOLPHER_PATH
+
+# make
 make html
 
 # upload the docs to GitHub if requested
@@ -22,4 +32,10 @@ if [[ "$@" == *"--upload"* ]]
 then
     ./upload_docs.sh && echo "Docs uploaded successfully." && exit
     echo && echo "There was an error during upload. See the messages above for more information." 1>&2
+fi
+
+# serve docs if required
+if [[ "$@" == *"--serve"* ]]
+then
+    python serve.py
 fi
---
+++ doc/serve.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+
+web_dir = os.path.join(os.path.dirname(__file__), 'build/html/')
+os.chdir(web_dir)
+
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at: localhost:", PORT, sep="")
+httpd.serve_forever()
