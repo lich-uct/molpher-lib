d76ca66170a2e1a51a91bf0990c87e9005c39906
martin-sicho <sicho.martin@gmail.com>
2018-05-04 16:02:28 +0200

    added a test module to aggregate all tests into

--- conda/molpher-lib/molpher-lib/run_test.py
+++ conda/molpher-lib/molpher-lib/run_test.py
@@ -1,23 +1,3 @@
-import unittest
+from molpher.tests import run
 
-testmodules = [
-    # TODO: automatically import all tests from the package
-    'molpher.core.tests.test_api',
-    'molpher.core.tests.test_morphing',
-    ]
-
-suite = unittest.TestSuite()
-
-for t in testmodules:
-    try:
-        # If the module defines a suite() function, call it to get the suite.
-        mod = __import__(t, globals(), locals(), ['suite'])
-        suitefn = getattr(mod, 'suite')
-        suite.addTest(suitefn())
-    except (ImportError, AttributeError):
-        # else, just load all the test cases from the module.
-        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
-
-result = unittest.TextTestRunner().run(suite)
-if result.errors:
-    exit(1)
+run()
---
+++ src/python/molpher/tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+TEST_MODULES = [
+    # TODO: automatically import all tests from the package
+    'molpher.core.tests.test_api',
+    'molpher.core.tests.test_morphing',
+]
+
+def run(module_list=TEST_MODULES):
+    suite = unittest.TestSuite()
+
+    for t in module_list:
+        try:
+            # If the module defines a suite() function, call it to get the suite.
+            mod = __import__(t, globals(), locals(), ['suite'])
+            suitefn = getattr(mod, 'suite')
+            suite.addTest(suitefn())
+        except (ImportError, AttributeError):
+            # else, just load all the test cases from the module.
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+    result = unittest.TextTestRunner().run(suite)
+    if result.errors:
+        exit(1)
