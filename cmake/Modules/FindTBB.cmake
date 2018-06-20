836ba8a5a89da1fa47626a6c775f2a99c513d24b
martin-sicho <sicho.martin@gmail.com>
2018-06-20 15:56:29 +0200

    new FindTBB file

--- cmake/Modules/FindTBB.cmake
+++ cmake/Modules/FindTBB.cmake
@@ -76,9 +76,19 @@
 #
 # The following varibles should be used to build and link with TBB:
 #
-# * TBB_INCLUDE_DIRS - The include directory for TBB.
-# * TBB_LIBRARIES    - The libraries to link against to use TBB.
-# * TBB_DEFINITIONS  - Definitions to use when compiling code that uses TBB.
+# * TBB_INCLUDE_DIRS        - The include directory for TBB.
+# * TBB_LIBRARIES           - The libraries to link against to use TBB.
+# * TBB_LIBRARIES_RELEASE   - The release libraries to link against to use TBB.
+# * TBB_LIBRARIES_DEBUG     - The debug libraries to link against to use TBB.
+# * TBB_DEFINITIONS         - Definitions to use when compiling code that uses
+#                             TBB.
+# * TBB_DEFINITIONS_RELEASE - Definitions to use when compiling release code that
+#                             uses TBB.
+# * TBB_DEFINITIONS_DEBUG   - Definitions to use when compiling debug code that
+#                             uses TBB.
+#
+# This module will also create the "tbb" target that may be used when building
+# executables and libraries.
 
 include(FindPackageHandleStandardArgs)
 
@@ -89,11 +99,15 @@ if(NOT TBB_FOUND)
     ##################################
 
     if(NOT DEFINED TBB_USE_DEBUG_BUILD)
-        if(CMAKE_BUILD_TYPE MATCHES "[Debug|DEBUG|debug|RelWithDebInfo|RELWITHDEBINFO|relwithdebinfo]")
-            set(TBB_USE_DEBUG_BUILD TRUE)
+        if(CMAKE_BUILD_TYPE MATCHES "(Debug|DEBUG|debug|RelWithDebInfo|RELWITHDEBINFO|relwithdebinfo)")
+            set(TBB_BUILD_TYPE DEBUG)
         else()
-            set(TBB_USE_DEBUG_BUILD FALSE)
+            set(TBB_BUILD_TYPE RELEASE)
         endif()
+    elseif(TBB_USE_DEBUG_BUILD)
+        set(TBB_BUILD_TYPE DEBUG)
+    else()
+        set(TBB_BUILD_TYPE RELEASE)
     endif()
 
     ##################################
@@ -138,7 +152,7 @@ if(NOT TBB_FOUND)
         # TODO: Check to see which C++ library is being used by the compiler.
         if(NOT ${CMAKE_SYSTEM_VERSION} VERSION_LESS 13.0)
             # The default C++ library on OS X 10.9 and later is libc++
-            set(TBB_LIB_PATH_SUFFIX "lib/libc++")
+            set(TBB_LIB_PATH_SUFFIX "lib/libc++" "lib")
         else()
             set(TBB_LIB_PATH_SUFFIX "lib")
         endif()
@@ -150,9 +164,9 @@ if(NOT TBB_FOUND)
         #       <arch>/gcc4.1. For now, assume that the compiler is more recent than
         #       gcc 4.4.x or later.
         if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
-            set(TBB_LIB_PATH_SUFFIX "lib/intel64/gcc4.4")
+            set(TBB_LIB_PATH_SUFFIX "lib/intel64/gcc4.7")
         elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^i.86$")
-            set(TBB_LIB_PATH_SUFFIX "lib/ia32/gcc4.4")
+            set(TBB_LIB_PATH_SUFFIX "lib/ia32/gcc4.7")
         endif()
     endif()
 
@@ -164,57 +178,6 @@ if(NOT TBB_FOUND)
             HINTS ${TBB_INCLUDE_DIR} ${TBB_SEARCH_DIR}
             PATHS ${TBB_DEFAULT_SEARCH_DIR}
             PATH_SUFFIXES include)
-
-    ##################################
-    # Find TBB components
-    ##################################
-
-    # Find each component
-    foreach(_comp tbb_preview tbbmalloc tbbmalloc_proxy tbb)
-        # Search for the libraries
-        find_library(TBB_${_comp}_LIBRARY_RELEASE ${_comp}
-                HINTS ${TBB_LIBRARY} ${TBB_SEARCH_DIR}
-                PATHS ${TBB_DEFAULT_SEARCH_DIR}
-                PATH_SUFFIXES "${TBB_LIB_PATH_SUFFIX}")
-
-        find_library(TBB_${_comp}_LIBRARY_DEBUG ${_comp}_debug
-                HINTS ${TBB_LIBRARY} ${TBB_SEARCH_DIR}
-                PATHS ${TBB_DEFAULT_SEARCH_DIR} ENV LIBRARY_PATH
-                PATH_SUFFIXES "${TBB_LIB_PATH_SUFFIX}")
-
-
-        # Set the library to be used for the component
-        if(NOT TBB_${_comp}_LIBRARY)
-            if(TBB_USE_DEBUG_BUILD AND TBB_${_comp}_LIBRARY_DEBUG)
-                set(TBB_${_comp}_LIBRARY "${TBB_${_comp}_LIBRARY_DEBUG}")
-            elseif(TBB_${_comp}_LIBRARY_RELEASE)
-                set(TBB_${_comp}_LIBRARY "${TBB_${_comp}_LIBRARY_RELEASE}")
-            elseif(TBB_${_comp}_LIBRARY_DEBUG)
-                set(TBB_${_comp}_LIBRARY "${TBB_${_comp}_LIBRARY_DEBUG}")
-            endif()
-        endif()
-
-        # Set the TBB library list and component found variables
-        if(TBB_${_comp}_LIBRARY)
-            list(APPEND TBB_LIBRARIES "${TBB_${_comp}_LIBRARY}")
-            set(TBB_${_comp}_FOUND TRUE)
-        else()
-            set(TBB_${_comp}_FOUND FALSE)
-        endif()
-
-        mark_as_advanced(TBB_${_comp}_LIBRARY_RELEASE)
-        mark_as_advanced(TBB_${_comp}_LIBRARY_DEBUG)
-        mark_as_advanced(TBB_${_comp}_LIBRARY)
-
-    endforeach()
-
-    ##################################
-    # Set compile flags
-    ##################################
-
-    if(TBB_tbb_LIBRARY MATCHES "debug")
-        set(TBB_DEFINITIONS "-DTBB_USE_DEBUG=1")
-    endif()
 
     ##################################
     # Set version strings
@@ -231,14 +194,109 @@ if(NOT TBB_FOUND)
         set(TBB_VERSION "${TBB_VERSION_MAJOR}.${TBB_VERSION_MINOR}")
     endif()
 
+    ##################################
+    # Find TBB components
+    ##################################
+
+    if(TBB_VERSION VERSION_LESS 4.3)
+        set(TBB_SEARCH_COMPOMPONENTS tbb_preview tbbmalloc tbb)
+    else()
+        set(TBB_SEARCH_COMPOMPONENTS tbb_preview tbbmalloc_proxy tbbmalloc tbb)
+    endif()
+
+    # Find each component
+    foreach(_comp ${TBB_SEARCH_COMPOMPONENTS})
+        if(";${TBB_FIND_COMPONENTS};tbb;" MATCHES ";${_comp};")
+
+            # Search for the libraries
+            find_library(TBB_${_comp}_LIBRARY_RELEASE ${_comp}
+                    HINTS ${TBB_LIBRARY} ${TBB_SEARCH_DIR}
+                    PATHS ${TBB_DEFAULT_SEARCH_DIR} ENV LIBRARY_PATH
+                    PATH_SUFFIXES ${TBB_LIB_PATH_SUFFIX})
+
+            find_library(TBB_${_comp}_LIBRARY_DEBUG ${_comp}_debug
+                    HINTS ${TBB_LIBRARY} ${TBB_SEARCH_DIR}
+                    PATHS ${TBB_DEFAULT_SEARCH_DIR} ENV LIBRARY_PATH
+                    PATH_SUFFIXES ${TBB_LIB_PATH_SUFFIX})
+
+            if(TBB_${_comp}_LIBRARY_DEBUG)
+                list(APPEND TBB_LIBRARIES_DEBUG "${TBB_${_comp}_LIBRARY_DEBUG}")
+            endif()
+            if(TBB_${_comp}_LIBRARY_RELEASE)
+                list(APPEND TBB_LIBRARIES_RELEASE "${TBB_${_comp}_LIBRARY_RELEASE}")
+            endif()
+            if(TBB_${_comp}_LIBRARY_${TBB_BUILD_TYPE} AND NOT TBB_${_comp}_LIBRARY)
+                set(TBB_${_comp}_LIBRARY "${TBB_${_comp}_LIBRARY_${TBB_BUILD_TYPE}}")
+            endif()
+
+            if(TBB_${_comp}_LIBRARY AND EXISTS "${TBB_${_comp}_LIBRARY}")
+                set(TBB_${_comp}_FOUND TRUE)
+            else()
+                set(TBB_${_comp}_FOUND FALSE)
+            endif()
+
+            # Mark internal variables as advanced
+            mark_as_advanced(TBB_${_comp}_LIBRARY_RELEASE)
+            mark_as_advanced(TBB_${_comp}_LIBRARY_DEBUG)
+            mark_as_advanced(TBB_${_comp}_LIBRARY)
+
+        endif()
+    endforeach()
+
+    ##################################
+    # Set compile flags and libraries
+    ##################################
+
+    set(TBB_DEFINITIONS_RELEASE "")
+    set(TBB_DEFINITIONS_DEBUG "-DTBB_USE_DEBUG=1")
+
+    if(TBB_LIBRARIES_${TBB_BUILD_TYPE})
+        set(TBB_DEFINITIONS "${TBB_DEFINITIONS_${TBB_BUILD_TYPE}}")
+        set(TBB_LIBRARIES "${TBB_LIBRARIES_${TBB_BUILD_TYPE}}")
+    elseif(TBB_LIBRARIES_RELEASE)
+        set(TBB_DEFINITIONS "${TBB_DEFINITIONS_RELEASE}")
+        set(TBB_LIBRARIES "${TBB_LIBRARIES_RELEASE}")
+    elseif(TBB_LIBRARIES_DEBUG)
+        set(TBB_DEFINITIONS "${TBB_DEFINITIONS_DEBUG}")
+        set(TBB_LIBRARIES "${TBB_LIBRARIES_DEBUG}")
+    endif()
+
     find_package_handle_standard_args(TBB
             REQUIRED_VARS TBB_INCLUDE_DIRS TBB_LIBRARIES
             HANDLE_COMPONENTS
             VERSION_VAR TBB_VERSION)
 
+    ##################################
+    # Create targets
+    ##################################
+
+    if(NOT CMAKE_VERSION VERSION_LESS 3.0 AND TBB_FOUND)
+        add_library(tbb SHARED IMPORTED)
+        set_target_properties(tbb PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES  ${TBB_INCLUDE_DIRS}
+                IMPORTED_LOCATION              ${TBB_LIBRARIES})
+        if(TBB_LIBRARIES_RELEASE AND TBB_LIBRARIES_DEBUG)
+            set_target_properties(tbb PROPERTIES
+                    INTERFACE_COMPILE_DEFINITIONS "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:TBB_USE_DEBUG=1>"
+                    IMPORTED_LOCATION_DEBUG          ${TBB_LIBRARIES_DEBUG}
+                    IMPORTED_LOCATION_RELWITHDEBINFO ${TBB_LIBRARIES_DEBUG}
+                    IMPORTED_LOCATION_RELEASE        ${TBB_LIBRARIES_RELEASE}
+                    IMPORTED_LOCATION_MINSIZEREL     ${TBB_LIBRARIES_RELEASE}
+                    )
+        elseif(TBB_LIBRARIES_RELEASE)
+            set_target_properties(tbb PROPERTIES IMPORTED_LOCATION ${TBB_LIBRARIES_RELEASE})
+        else()
+            set_target_properties(tbb PROPERTIES
+                    INTERFACE_COMPILE_DEFINITIONS "${TBB_DEFINITIONS_DEBUG}"
+                    IMPORTED_LOCATION              ${TBB_LIBRARIES_DEBUG}
+                    )
+        endif()
+    endif()
+
     mark_as_advanced(TBB_INCLUDE_DIRS TBB_LIBRARIES)
 
     unset(TBB_ARCHITECTURE)
+    unset(TBB_BUILD_TYPE)
     unset(TBB_LIB_PATH_SUFFIX)
     unset(TBB_DEFAULT_SEARCH_DIR)
 
